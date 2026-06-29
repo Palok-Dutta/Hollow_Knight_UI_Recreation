6555e034dd43b9092f234b812bfdfa6d81070863
Palok Dutta <palokdutta2004@gmail.com>
2026-06-29 23:11:57 +0600

    new scripts to manage ui more usefully

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,10 +7,20 @@ public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private List<AudioSource> m_HoverAudioSouces;
     public static MainMenuController Instance;
-
+    [SerializeField] private PanelUI[] m_Panels;
     private void Awake()
     {
         Instance = this;
+    }
+    
+    private void WireUpButtons()
+    {
+        foreach (var panel in m_Panels)
+        {
+            var type = panel.m_UIType; // capture for the closure
+            if (panel.m_CorrespondingButton == null) continue;
+            panel.m_CorrespondingButton.onClick.AddListener(() => OnClick(type));
+        }
     }
 
     public void PlayHoverSound()
@@ -25,5 +35,41 @@ public class MainMenuController : MonoBehaviour
         }
         m_HoverAudioSouces[0].Stop();
         m_HoverAudioSouces[0].Play();
+    }
+
+    private void Start()
+    {
+        HideAll();
+        DefaultOn();
+        WireUpButtons();
+    }
+
+    private void DefaultOn()
+    {
+        m_Panels[0].gameObject.SetActive(true);
+    }
+
+    public void OnClick(UIType type)
+    {
+        HideAll();
+        switch (type)
+        {
+            case  UIType.GameStart:
+                m_Panels[1].gameObject.SetActive(true);
+                break;
+            case  UIType.MainMenu:
+                m_Panels[0].gameObject.SetActive(true);
+                break;
+            case  UIType.Options:
+                m_Panels[2].gameObject.SetActive(true);
+                break;
+        }
+    }
+
+    private void HideAll()
+    {
+        m_Panels[0].gameObject.SetActive(false);
+        m_Panels[1].gameObject.SetActive(false);
+        m_Panels[2].gameObject.SetActive(false);
     }
 }
